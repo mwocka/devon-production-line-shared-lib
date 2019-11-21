c4069b4d213a95f7e447bd49505697d098add485
Mateusz Wocka <mateusz.wocka@gmail.com>
2019-11-21 15:23:29 +0100

    Update JenkinsConfiguration.groovy

--- src/com/capgemini/productionline/configuration/JenkinsConfiguration.groovy
+++ src/com/capgemini/productionline/configuration/JenkinsConfiguration.groovy
@@ -833,9 +833,7 @@ class JenkinsConfiguration implements Serializable {
 	        //def configStore = Jenkins.get().getExtensionList('org.jenkinsci.plugins.configfiles.GlobalConfigFiles')[0]
 	        
 	        //def cfg = configStore.getById(defaultConfigID)
-            def cfg = '''
-                `<?xml version="1.0" encoding="UTF-8"?>`
-                <settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
+            def cfg = '''<settings xmlns="http://maven.apache.org/SETTINGS/1.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/SETTINGS/1.0.0 http://maven.apache.org/xsd/settings-1.0.0.xsd">
                   <pluginGroups/>
                   <proxies/>
                   <servers/>
